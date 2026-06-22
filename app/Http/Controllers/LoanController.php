fcc1ea4120589f096795358eaf635fc9015511a5
BASSAM <egsmartline@gmail.com>
2026-06-22 18:04:05 +0300

    fix: use max id for loan_number (root)

--- app/Http/Controllers/LoanController.php
+++ app/Http/Controllers/LoanController.php
@@ -49,11 +49,8 @@ class LoanController extends TenantAwareController
         $validated['status'] = 'active';
         $validated['remaining'] = $validated['amount'];
 
-        $maxLoan = Loan::where('tenant_id', $this->getTenantId())
-            ->where('loan_number', 'like', 'L-%')
-            ->orderByRaw('CAST(SUBSTRING(loan_number, 3) AS UNSIGNED) DESC')
-            ->first();
-        $nextNumber = $maxLoan ? intval(substr($maxLoan->loan_number, 2)) + 1 : 1;
+        $maxId = Loan::where('tenant_id', $this->getTenantId())->max('id');
+        $nextNumber = ($maxId ?? 0) + 1;
         $validated['loan_number'] = 'L-' . str_pad($nextNumber, 5, '0', STR_PAD_LEFT);
 
         Loan::create($validated);
