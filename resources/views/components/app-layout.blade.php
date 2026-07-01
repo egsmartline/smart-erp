c0233027011c21a8cf641f3c0dc91a864fcda889
BASSAM <egsmartline@gmail.com>
2026-07-01 10:53:20 +0300

    إضافة darkMode: class لتفعيل الوضع الليلي

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -31,6 +31,7 @@
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <script>
         tailwind.config = {
+            darkMode: 'class',
             theme: {
                 extend: {
                     fontFamily: { 'cairo': ['Cairo', 'sans-serif'] },
